0d115697637e396bb6cfce5a2a4fc38955d2b959
Ateina <totora246@gmail.com>
2016-11-13 20:55:41 +0300

    Method Monte Carlo using MathNet.Numericd Library

---
+++ Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MathNet.Numerics.Random;
+using MathNet.Numerics.Distributions;
+
+namespace IiSM4Lab
+{
+    class Program
+    {
+        public static double f(double x)
+        {
+        if (x < 1)
+            {
+                return 0;
+            }
+        else
+            {
+                return Math.Pow(Math.Sin(x), 5);
+            }
+        }
+
+        public static double var_x() 
+        {
+            var exp = new Exponential(1);
+            double randomSample = exp.Sample();
+            //Console.WriteLine(exp + "   " + randomSample);
+            return randomSample;
+        }
+
+        public static double integral()
+        {
+            int size = 10000;
+            double integrale = 0.0;
+            for (int i = 0; i < size; i++)
+            {
+                double x = var_x();
+                integrale += f(x);
+            }
+            return integrale/size;
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine(integral());
+            Console.ReadLine();
+        }
+    }
+}
