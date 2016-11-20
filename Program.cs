e600214fdd7f754d60906566e286c3c5dfec30f9
Ateina <totora246@gmail.com>
2016-11-20 17:58:48 +0300

    added func for resolve algebr system

--- Program.cs
+++ Program.cs
@@ -77,10 +77,93 @@ namespace IiSM4Lab
         }
         #endregion
 
+        #region система
+        static double[,] a = new double[,] {{0.7, -0.2},
+                                            {0.5, -0.5}};
+        static double[] f = { 1, 2 };
+        static double[] pi = {0.5, 0.5};
+        public static double[,] P = new double [2,2];
+        
+        static int N = 10000;
+        static int m = 100000;
+
+        //static int[] i = new int[N + 1];
+        //static double[] Q = new double[N + 1];
+        //static double[] ksi = new double[m];
+        static double alpha;
+
+        static double countVariable(int[] h)
+        {
+            int[] i = new int[N + 1];
+            double[] ksi = new double[m];
+            double[] Q = new double[N + 1];
+            for (int e = 0; e < 2; e++)
+            {
+                for (int j = 0; j < 2; j++)
+                {
+                    P[e, j] = 0.5;
+                }
+            }
+
+            double x = 0.0;
+            Random r = new Random();
+            //m реализаций цепи маркова
+            //вспомогательная цепь маркова
+            for (int k = 0; k < m; k++)
+            {
+                alpha = r.NextDouble();
+                if (alpha < 0.5)
+                    i[0] = 0;
+                else i[0] = 1;
+                for (int j = 1; j <= N; j++)
+                {
+                    alpha = r.NextDouble();
+                    if (alpha < 0.5)
+                        i[j] = 0;
+                    else i[j] = 1;
+                }
+                //веса
+                if (pi[0] > 0)
+                    Q[0] = h[i[0]] / pi[i[0]];
+                else Q[0] = 0;
+                for (int j = 1; j <= N; j++)
+                {
+                    int t1 = i[j - 1];
+                    int t2 = i[j];
+                    if (P[t1, t2] > 0.0)
+                    {
+                        Q[j] = Q[j - 1] * a[t1, t2] / P[t1, t2];
+                    }
+                    else Q[j] = 0;
+                }
+                //СВ
+                for (int j = 0; j <= N; j++)
+                    ksi[j] += Q[j] * f[i[j]];
+            }
+            for (int j = 0; j < m; j++)
+                x += ksi[j];
+            //Arrays.fill(i, 0);
+            //Arrays.fill(Q, 0.0);
+            //Arrays.fill(ksi, 0.0);
+            return x / m;
+        }
+
+        public static void Resolve_System()
+        {
+            double x = countVariable(new int[]{1, 0});
+            Console.WriteLine("x = " + x);
+            double y = countVariable(new int[]{0, 1});
+            Console.WriteLine("y = " + y);
+            Console.WriteLine();
+        }
+        #endregion
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Standard integral  :  " + Standard_integral());
-            Console.WriteLine("Double integral  :  " + Double_integral());
+            //Console.WriteLine("Standard integral  :  " + Standard_integral());
+            //Console.WriteLine("Double integral  :  " + Double_integral());
+
+            Resolve_System();
             Console.ReadLine();
         }
     }
